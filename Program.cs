9a63c6bc340dc206b0f1c76d0cb9dc309a9026f6
Robert Sendys <s29640@pjwstk.edu.pl>
2026-03-29 15:01:10 +0200

    Add input parsing in Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter numbers separated by spaces:");
+
+string? input = Console.ReadLine();
+
+Console.WriteLine($"You entered: {input}");
